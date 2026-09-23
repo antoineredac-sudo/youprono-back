cdccd48a9e1bf223dd2796d87cf56155f56f6012
antoineredac-sudo <antoine.redac@gmail.com>
2026-09-23 20:55:12 +0200

    Add files via upload

--- goldenfan-back-develop/dotnet.core.thegoldenfan/Dbs/GroupMember.cs
+++ goldenfan-back-develop/dotnet.core.thegoldenfan/Dbs/GroupMember.cs
@@ -12,6 +12,13 @@ public partial class GroupMember
 
     public DateTime DateJoined { get; set; }
 
+    // Qui l'a inscrit, quand ce n'est pas lui-meme : le createur du tournoi qui
+    // est alle le chercher dans le classement general (23 septembre 2026).
+    public Guid? AddedByUserId { get; set; }
+
+    // Vrai des qu'on le lui a dit. Le bandeau d'annonce ne se montre qu'une fois.
+    public bool NoticeSeen { get; set; }
+
     public virtual Group Group { get; set; } = null!;
 
     public virtual User User { get; set; } = null!;
